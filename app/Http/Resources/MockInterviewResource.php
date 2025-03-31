fbac8c851fce434ff9ce0406e16fc78eb5e56cb0
Ghada-Shaban <ghadaashaban2003@gmail.com>
2025-03-31 05:07:00 +0200

    Update MockInterviewResource.php

--- app/Http/Resources/MockInterviewResource.php
+++ app/Http/Resources/MockInterviewResource.php
@@ -12,7 +12,9 @@ class MockInterviewResource extends JsonResource
             'service_id' => $this->service_id,
             'interview_type' => $this->mockInterview ? $this->mockInterview->interview_type : null,
             'interview_level' => $this->mockInterview ? $this->mockInterview->interview_level : null,
-            'price' => $this->price ? $this->price->price : null,
+            'price' => $this->whenLoaded('price', function () {
+    return $this->price->price ?? null;
+}),
             'duration'=>'60 minutes',
             'no.of sessions'=>'1 session',
             'frequency'=> 'Weekly'
