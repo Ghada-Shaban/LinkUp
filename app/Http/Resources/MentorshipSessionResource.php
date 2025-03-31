b701e171cbd4c25ddf2210607e1f8c4028a73d47
Ghada-Shaban <ghadaashaban2003@gmail.com>
2025-03-31 05:07:16 +0200

    Update MentorshipSessionResource.php

--- app/Http/Resources/MentorshipSessionResource.php
+++ app/Http/Resources/MentorshipSessionResource.php
@@ -11,7 +11,9 @@ class MentorshipSessionResource extends JsonResource
         return [
             'service_id' => $this->service_id,
             'session_type' => $this->mentorship->mentorshipSession ? $this->mentorship->mentorshipSession->session_type : null,
-            'price' => $this->price ? $this->price->price : null,
+            'price' => $this->whenLoaded('price', function () {
+    return $this->price->price ?? null;
+}),
             'duration'=>'60 minutes',
             'no.of sessions'=>'1 session',
             'frequency'=> 'Weekly'
