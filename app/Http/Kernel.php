3ee8ad9dd0d210c58ea8311a2151b1a316309c60
Bassam Ahmed <bassaam529@gmail.com>
2025-04-01 15:57:11 +0200

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,9 +21,6 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        
-        
-
     ];
 
     /**
@@ -45,7 +42,6 @@ class Kernel extends HttpKernel
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-          
         ],
     ];
 
@@ -69,7 +65,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'check.coach.ownership' => \App\Http\Middleware\CheckCoachOwnership::class, 
-    'check.trainee' => \App\Http\Middleware\CheckTrainee::class,
-];
+        'check.trainee' => \App\Http\Middleware\CheckTrainee::class,
     ];
 }
+
