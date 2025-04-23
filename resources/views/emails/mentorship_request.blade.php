1ea56fb533bbee050cabeab65cbaf758c581e534
Bassam Ahmed <bassaam529@gmail.com>
2025-04-23 19:16:47 +0200

    Update mentorship_request.blade.php

--- resources/views/emails/mentorship_request.blade.php
+++ resources/views/emails/mentorship_request.blade.php
@@ -1,1 +1,15 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+    <title>New Mentorship Request</title>
+</head>
+<body>
+    <h1>New Mentorship Request</h1>
+    <p><strong>Trainee:</strong> {{ $mentorshipRequest->trainee->user->name }}</p>
+    <p><strong>Service:</strong> {{ $mentorshipRequest->service->name }}</p>
+    <p><strong>Type:</strong> {{ $mentorshipRequest->type }}</p>
+    <p><strong>First Session:</strong> {{ $mentorshipRequest->first_session_time }}</p>
+    <p><strong>Duration:</strong> {{ $mentorshipRequest->duration_minutes }} minutes</p>
+    <p>Please review the request in your dashboard.</p>
+</body>
+</html>
