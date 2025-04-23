df865179b9c6c406b7caf77a3fb148080d921540
Bassam Ahmed <bassaam529@gmail.com>
2025-04-23 16:31:32 +0200

    Create request_rejected.blade.php

---
+++ resources/views/emails/request_rejected.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mentorship Request Rejected</title>
+</head>
+<body>
+    <h1>Mentorship Request Rejected</h1>
+    <p>Dear Trainee,</p>
+    <p>We regret to inform you that your mentorship request for "<strong>{{ $title }}</strong>" (Type: {{ $type }}) has been rejected.</p>
+    <p>Better luck next time! Please try booking another slot.</p>
+    <p>Best regards,<br>Your Mentorship Platform</p>
+</body>
+</html>
