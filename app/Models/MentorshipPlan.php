725146d24322e2c4e1f24ff1984e26d1142c3eed
alaaahmed30 <alaaahmedd9070@gmail.com>
2025-04-24 21:51:57 +0200

    Update MentorshipPlan.php

--- app/Models/MentorshipPlan.php
+++ app/Models/MentorshipPlan.php
@@ -19,4 +19,20 @@ class MentorshipPlan extends Model
     {
         return $this->belongsTo(Mentorship::class, 'service_id', 'service_id');
     }
+    public function mentorship()
+    {
+        return $this->belongsTo(Mentorship::class, 'service_id', 'service_id');
+    }
+    public function requests()
+    {
+        return $this->morphMany(MentorshipRequest::class, 'requestable');
+    }
+    public function service()
+    {
+        return $this->belongsTo(Service::class, 'service_id');
+    }
+    public function coach()
+    {
+        return $this->service->coach();
+    }
 }
