1307c6f6ded2f48815fa74bdd5d72dd2ea156363
Bassam Ahmed <bassaam529@gmail.com>
2025-04-26 01:51:25 +0300

    Update GroupMentorship.php

--- app/Models/GroupMentorship.php
+++ app/Models/GroupMentorship.php
@@ -18,28 +18,56 @@ class GroupMentorship extends Model
         'description', 
         'day',
         'start_time',
-    'available_slots',
-    'current_participants',
-    'trainee_ids',
-       
-    
+        'available_slots',
+        'current_participants',
+        'trainee_ids',
+    ];
+
+    protected $casts = [
+        'trainee_ids' => 'array',
     ];
 
     public function service()
     {
         return $this->belongsTo(Service::class, 'service_id', 'service_id');
     }
-public function getAvailableSlotsAttribute()
+
+    public function getAvailableSlotsAttribute()
     {
         return $this->max_participants - $this->current_participants;
     }
+
     public function requests()
     {
         return $this->morphMany(MentorshipRequest::class, 'requestable');
     }
+
     public function coach()
     {
         return $this->service->coach();
     }
+
+    public function addTrainee($traineeId)
+    {
+        $traineeIds = $this->trainee_ids ?? [];
+        if (!in_array($traineeId, $traineeIds)) {
+            $traineeIds[] = $traineeId;
+            $this->trainee_ids = $traineeIds;
+            $this->increment('current_participants');
+            $this->save();
+        }
+    }
+
+    public function removeTrainee($traineeId)
+    {
+        $traineeIds = $this->trainee_ids ?? [];
+        if (($key = array_search($traineeId, $traineeIds)) !== false) {
+            unset($traineeIds[$key]);
+            $this->trainee_ids = array_values($traineeIds);
+            $this->decrement('current_participants');
+            $this->save();
+        }
+    }
 }
+
 
