59a59db3a395d03b26ff7168e3441bb000c4eb89
Bassam Ahmed <bassaam529@gmail.com>
2025-04-26 14:41:46 +0300

    Update RequestAccepted.php

--- app/Mail/RequestAccepted.php
+++ app/Mail/RequestAccepted.php
@@ -6,7 +6,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class MentorshipRequestAccepted extends Mailable
+class RequestAccepted extends Mailable
 {
     use Queueable, SerializesModels;
 
