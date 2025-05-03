16ebbbc252a1c91eb8c81fff4bc68c38a4f6af8a
Ghada-Shaban <ghadaashaban2003@gmail.com>
2025-05-03 03:06:54 +0300

    Update Payment.php

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -35,8 +35,8 @@ class Payment extends Model
         return $this->belongsTo(NewSession::class, 'session_id', 'new_session_id');
     }
 
-   // public function service()
-   //  {
-   //      return $this->belongsTo(Service::class, 'service_id', 'service_id');
-   //  }
+   public function service()
+     {
+         return $this->belongsTo(Service::class, 'service_id', 'service_id');
+    }
 }
