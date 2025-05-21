b30fc143c59ba55203ae5b9eb38b8886de7a459f
Bassam Ahmed <bassaam529@gmail.com>
2025-05-21 18:47:40 +0300

    Update request_rejected.blade.php

--- resources/views/emails/request_rejected.blade.php
+++ resources/views/emails/request_rejected.blade.php
@@ -1,12 +1,15 @@
 @component('mail::message')
-
 # Mentorship Request Rejected
 
-Hello {{ $mentorshipRequest->trainee->name }},
+Hello {{ $mentorshipRequest->trainee->full_name }},
 
 Unfortunately, your mentorship request for **{{ $mentorshipRequest->requestable->title }}** has been rejected by the coach.
 
 You can try submitting another request or contact the coach for more details.
+
+@component('mail::button', ['url' => url('/trainee/mentorship-requests')])
+Submit Another Request
+@endcomponent
 
 Thanks,  
 {{ config('app.name') }}
