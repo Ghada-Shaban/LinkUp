62a5ce8877cfa2a800261f26f1c490eeae2adbb6
Bassam Ahmed <bassaam529@gmail.com>
2025-05-21 17:22:27 +0300

    Update and rename RequestAccepted.php to GroupMentorshipRequestAccepted.php

--- app/Mail/GroupMentorshipRequestAccepted.php
+++ app/Mail/GroupMentorshipRequestAccepted.php
@@ -5,21 +5,22 @@ namespace App\Mail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
+use App\Models\MentorshipRequest;
 
-class RequestAccepted extends Mailable
+class GroupMentorshipRequestAccepted extends Mailable
 {
     use Queueable, SerializesModels;
 
     public $mentorshipRequest;
 
-    public function __construct($mentorshipRequest)
+    public function __construct(MentorshipRequest $mentorshipRequest)
     {
         $this->mentorshipRequest = $mentorshipRequest;
     }
 
     public function build()
     {
-        return $this->subject('Mentorship Request Accepted')
-                    ->view('emails.mentorship-request-accepted');
+        return $this->subject('Group Mentorship Request Accepted')
+                    ->view('emails.group_mentorship_request_accepted');
     }
 }
