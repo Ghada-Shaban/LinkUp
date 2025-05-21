82333317c9dd8aa63fc1094795d1ada48b575dc3
Bassam Ahmed <bassaam529@gmail.com>
2025-05-21 17:26:14 +0300

    Create MentorshipPlanRequestAccepted.php

---
+++ app/Mail/MentorshipPlanRequestAccepted.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use App\Models\MentorshipRequest;
+
+class MentorshipPlanRequestAccepted extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $mentorshipRequest;
+
+    public function __construct(MentorshipRequest $mentorshipRequest)
+    {
+        $this->mentorshipRequest = $mentorshipRequest;
+    }
+
+    public function build()
+    {
+        return $this->subject('Mentorship Plan Request Accepted')
+                    ->view('emails.mentorship_plan_request_accepted');
+    }
+}
