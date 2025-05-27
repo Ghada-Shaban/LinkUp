b72862c3c522f4aea27adecc8979ce2e0a85d895
Bassam Ahmed <bassaam529@gmail.com>
2025-05-27 22:59:10 +0300

    Update MentorshipRequest.php

--- app/Models/MentorshipRequest.php
+++ app/Models/MentorshipRequest.php
@@ -4,8 +4,6 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
-// إضافة use للكلاسات GroupMentorship و MentorshipPlan
 use App\Models\GroupMentorship;
 use App\Models\MentorshipPlan;
 
