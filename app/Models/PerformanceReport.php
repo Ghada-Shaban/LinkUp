91eddce830661f013bce2aecda51343a12bcc410
Ghada-Shaban <ghadaashaban2003@gmail.com>
2025-05-27 15:49:16 +0300

    Add files via upload

---
+++ app/Models/PerformanceReport.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PerformanceReport extends Model
+{
+    protected $table = 'performance_reports';
+    protected $fillable = [
+        'session_id', 'coach_id', 'trainee_id', 'overall_rating', 'strengths', 'weaknesses', 'comments'
+    ];
+
+    public function session()
+    {
+        return $this->belongsTo(NewSession::class, 'session_id', 'new_session_id');
+    }
+
+    public function coach()
+    {
+        return $this->belongsTo(User::class, 'coach_id', 'User_ID');
+    }
+
+    public function trainee()
+    {
+        return $this->belongsTo(User::class, 'trainee_id', 'User_ID');
+    }
+}
