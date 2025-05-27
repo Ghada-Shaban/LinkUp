8787c430b7b7a7786a920c1d0f4718967f095240
Bassam Ahmed <bassaam529@gmail.com>
2025-05-27 22:56:40 +0300

    Update Admin.php

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -18,7 +18,7 @@ class Admin extends Authenticatable
     protected $fillable = [
         'Full_Name',
         'Email',
-        'Password' // هنا كمان اتأكد إنه Password
+        'Password' 
     ];
 
     protected $hidden = [
