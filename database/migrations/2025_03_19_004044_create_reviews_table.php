37b7c5a66555c6c1efbe14c6efa5d41034254065
Ghada-Shaban <ghadaashaban2003@gmail.com>
2025-05-31 03:50:14 +0300

    Update 2025_03_19_004044_create_reviews_table.php

--- database/migrations/2025_03_19_004044_create_reviews_table.php
+++ database/migrations/2025_03_19_004044_create_reviews_table.php
@@ -7,22 +7,22 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
    
-    public function up(): void
+     public function up(): void
     {
         Schema::create('reviews', function (Blueprint $table) {
+            $table->id(); 
             $table->unsignedBigInteger('trainee_id');
             $table->unsignedBigInteger('coach_id');
             $table->enum('rating', ['1', '2', '3', '4', '5']);
             $table->text('comment')->nullable();
             $table->timestamps();
 
-            $table->primary(['trainee_id', 'coach_id']);
+          
             $table->foreign('trainee_id')->references('User_ID')->on('trainees')->onDelete('cascade');
             $table->foreign('coach_id')->references('User_ID')->on('coaches')->onDelete('cascade');
         });
     }
 
-   
     public function down(): void
     {
         Schema::dropIfExists('reviews');
