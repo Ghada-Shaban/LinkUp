8a9cebf48139442c4fc839739f3f120d719fccf2
Ghada-Shaban <ghadaashaban2003@gmail.com>
2025-06-09 02:00:46 +0300

    Update ServiceResource.php

--- app/Http/Resources/ServiceResource.php
+++ app/Http/Resources/ServiceResource.php
@@ -11,50 +11,48 @@ class ServiceResource extends JsonResource
         $data = [
             'service_id' => $this->service_id,
             'service_type' => $this->service_type,
-            'price' => $this->price ? $this->price->price : null,
+            'price' => $this->whenLoaded('price', fn () => $this->price->price),
         ];
 
-        if ($this->service_type === 'Mentorship') {
-            if ($this->mentorship) {
-                if ($this->mentorship->mentorship_type === 'Mentorship plan' && $this->mentorship->mentorshipPlan) {
-                    $data['mentorship'] = [
-                        'mentorship_plan' => [
-                            'title' => $this->mentorship->mentorshipPlan->title,
-                            'duration' => '60 minutes',
-                            'no_of_sessions' => '4 sessions',
-                        ]
-                    ];
-                } else if ($this->mentorship->mentorship_type === 'Mentorship session' && $this->mentorship->mentorshipSession) {
-                    $data['mentorship'] = [
-                        'mentorship_session' => [
-                            'session_type' => $this->mentorship->mentorshipSession->session_type,
-                            'duration' => '60 minutes',
-                            'no_of_sessions' => '1 session',
-                        ]
-                    ];
-                }
+        if ($this->relationLoaded('mentorship') && $this->mentorship) {
+            if ($this->mentorship->mentorship_type === 'Mentorship plan' && $this->relationLoaded('mentorship.mentorshipPlan') && $this->mentorship->mentorshipPlan) {
+                $data['mentorship'] = [
+                    'mentorship_plan' => [
+                        'title' => $this->mentorship->mentorshipPlan->title ?? null,
+                        'duration' => '60 minutes',
+                        'no_of_sessions' => '4 sessions',
+                    ],
+                ];
+            } elseif ($this->mentorship->mentorship_type === 'Mentorship session' && $this->relationLoaded('mentorship.mentorshipSession') && $this->mentorship->mentorshipSession) {
+                $data['mentorship'] = [
+                    'mentorship_session' => [
+                        'session_type' => $this->mentorship->mentorshipSession->session_type ?? null,
+                        'duration' => '60 minutes',
+                        'no_of_sessions' => '1 session',
+                    ],
+                ];
             }
         }
 
-        if ($this->service_type === 'Group_Mentorship' && $this->groupMentorship) {
+        if ($this->relationLoaded('groupMentorship') && $this->groupMentorship) {
             $data['group_mentorship'] = [
-                'title' => $this->groupMentorship->title,
-                'description' => $this->groupMentorship->description,
-                'day' => $this->groupMentorship->day,
-                'start_time' => $this->groupMentorship->start_time,
+                'title' => $this->groupMentorship->title ?? null,
+                'description' => $this->groupMentorship->description ?? null,
+                'day' => $this->groupMentorship->day ?? null,
+                'start_time' => $this->groupMentorship->start_time ?? null,
                 'duration' => '60 minutes',
                 'no_of_sessions' => '4 sessions',
                 'min_participants' => $this->groupMentorship->min_participants ?? 2,
                 'max_participants' => $this->groupMentorship->max_participants ?? 5,
                 'available_slots' => $this->groupMentorship->available_slots ?? 
-                    (($this->groupMentorship->max_participants ?? 5) - ($this->groupMentorship->current_participants ?? 0))
+                    (($this->groupMentorship->max_participants ?? 5) - ($this->groupMentorship->current_participants ?? 0)),
             ];
         }
 
-        if ($this->service_type === 'Mock_Interview' && $this->mockInterview) {
+        if ($this->relationLoaded('mockInterview') && $this->mockInterview) {
             $data['mock_interview'] = [
-                'interview_type' => $this->mockInterview->interview_type,
-                'interview_level' => $this->mockInterview->interview_level,
+                'interview_type' => $this->mockInterview->interview_type ?? null,
+                'interview_level' => $this->mockInterview->interview_level ?? null,
                 'duration' => '60 minutes',
                 'no_of_sessions' => '1 session',
             ];
