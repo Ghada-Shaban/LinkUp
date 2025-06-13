ace88134fee6dbdf4154905bd33149162de6cefc
Ghada-Shaban <ghadaashaban2003@gmail.com>
2025-06-13 17:00:53 +0300

    Update CoachDashboardController.php

--- app/Http/Controllers/Api/CoachDashboardController.php
+++ app/Http/Controllers/Api/CoachDashboardController.php
@@ -103,7 +103,7 @@ class CoachDashboardController extends Controller
                         }
                     }
                         
-                    $trainee = $session->trainee;
+                    $trainee = $session->trainees;
                     $traineeName = $trainee ? $trainee->Full_Name : 'Unknown Trainee';
                     $traineePhoto = $trainee ? ($trainee->photo ?? null) : null;
 
